9d26ceb52095a831d15fa8b754753dee3b32565b
unknown <hadycas@gmail.com>
2026-01-06 08:26:10 +0900

    가자

--- transactional-emails/emails/confirm-email.tsx
+++ transactional-emails/emails/confirm-email.tsx
@@ -42,14 +42,14 @@ export default function ConfirmEmail() {
                 {`http://localhost:5173/auth/confirm?token_hash={{ .TokenHash }}&type=email&next=/`}
               </Text>
               <Text className="mb-4 text-[15px] leading-relaxed text-black">
-                If you did not create an account with Supaplate, you can safely
-                ignore this email.
+                If you did not create an account with Richroutine, you can
+                safely ignore this email.
               </Text>
               <Text className="mb-4 text-[15px] leading-relaxed text-black">
                 Best regards,
               </Text>
               <Text className="mb-4 text-[15px] leading-relaxed text-black">
-                The Supaplate Team
+                The Richroutine Team
               </Text>
             </Section>
           </Container>
