a677b5ee7edee8371dac76c73a31666b32cc45e4
jhkim5 <jhkim5@ypad.co.kr>
2026-01-23 16:33:21 +0900

    home  진행중

--- app/features/home/screens/home.tsx
+++ app/features/home/screens/home.tsx
@@ -13,11 +13,17 @@
  */
 import type { Route } from "./+types/home";
 
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 import { AuroraText } from "~/core/components/ui/aurora-text";
 import { Button } from "~/core/components/ui/button";
+import { Marquee } from "~/core/components/ui/marquee";
+import { NumberTicker } from "~/core/components/ui/number-ticker";
+import { Progress } from "~/core/components/ui/progress";
+import { TextAnimate } from "~/core/components/ui/text-animate";
 import i18next from "~/core/lib/i18next.server";
+import { cn } from "~/core/lib/utils";
 
 /**
  * Meta function for setting page metadata
@@ -85,23 +91,235 @@ export async function loader({ request }: Route.LoaderArgs) {
  * @returns JSX element representing the home page
  */
 export default function Home() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500);
+    return () => clearTimeout(timer);
+  }, []);
   // Get the translation function for the current locale
   const { t } = useTranslation();
 
+  const reviews = [
+    {
+      name: "Jack",
+      username: "@jack",
+      body: "I've never seen anything like this before. It's amazing. I love it.",
+      img: "https://avatar.vercel.sh/jack",
+    },
+    {
+      name: "Jill",
+      username: "@jill",
+      body: "I don't know what to say. I'm speechless. This is amazing.",
+      img: "https://avatar.vercel.sh/jill",
+    },
+    {
+      name: "John",
+      username: "@john",
+      body: "I'm at a loss for words. This is amazing. I love it.",
+      img: "https://avatar.vercel.sh/john",
+    },
+    {
+      name: "Jane",
+      username: "@jane",
+      body: "I'm at a loss for words. This is amazing. I love it.",
+      img: "https://avatar.vercel.sh/jane",
+    },
+    {
+      name: "Jenny",
+      username: "@jenny",
+      body: "I'm at a loss for words. This is amazing. I love it.",
+      img: "https://avatar.vercel.sh/jenny",
+    },
+    {
+      name: "James",
+      username: "@james",
+      body: "I'm at a loss for words. This is amazing. I love it.",
+      img: "https://avatar.vercel.sh/james",
+    },
+  ];
+
+  const firstRow = reviews.slice(0, reviews.length / 2);
+  const ReviewCard = ({
+    img,
+    name,
+    username,
+    body,
+  }: {
+    img: string;
+    name: string;
+    username: string;
+    body: string;
+  }) => {
+    return (
+      <figure
+        className={cn(
+          "relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
+          // light styles
+          "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+          // dark styles
+          "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+        )}
+      >
+        <div className="flex flex-row items-center gap-2">
+          <img
+            className="rounded-full"
+            width="32"
+            height="32"
+            alt=""
+            src={img}
+          />
+          <div className="flex flex-col">
+            <figcaption className="text-sm font-medium dark:text-white">
+              {name}
+            </figcaption>
+            <p className="text-xs font-medium dark:text-white/40">{username}</p>
+          </div>
+        </div>
+        <blockquote className="mt-2 text-sm">{body}</blockquote>
+      </figure>
+    );
+  };
+
   return (
     <>
-      <div className="flex flex-col gap-3">
-        <h1 className="text-4xl font-extrabold tracking-tight lg:text-6xl">
-          <AuroraText>리치루틴</AuroraText>
-        </h1>
-        <h2 className="text-muted-foreground text-xl">
-          상위 1% 자본가의 루틴은 아침부터 시작됩니다.
-          <br />
-          미국/국내 주식부터 부동산까지, AI가 요약한 뉴스를 쉽게 만나보세요.
-        </h2>
+      {/* 히어로 섹션 */}
+      <div className="grid grid-cols-2">
+        <div className="flex flex-col gap-3">
+          <div>
+            <h1 className="text-5xl font-extrabold tracking-tight lg:text-7xl">
+              <AuroraText>리치루틴</AuroraText>
+            </h1>
+            <h2 className="text-muted-foreground text-xl">
+              상위 1% 자본가의 루틴은 아침부터 시작됩니다.
+              <br />
+              미국/국내 주식부터 부동산까지, AI가 요약한 뉴스를 쉽게 만나보세요.
+            </h2>
+          </div>
+          <div className="mt-6">
+            <Button size="lg" className="font-semibold">
+              무료로 시작하기
+            </Button>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex w-full max-w-sm flex-col gap-3">
+            <span className="text-center text-4xl font-semibold tracking-tight">
+              2026년 회원수 목표
+            </span>
+            <Progress value={progress} className="relative h-8 shadow-sm">
+              <div className="pointer-events-none absolute inset-0 flex items-center justify-center gap-1">
+                <div>
+                  <NumberTicker
+                    value={500}
+                    className="text-primary-foreground font-bold"
+                    delay={0.5}
+                  />
+                  <span className="text-primary-foreground font-bold">명</span>
+                </div>
+                <div>
+                  <span className="text-primary-foreground font-bold">(</span>
+                  <NumberTicker
+                    value={50}
+                    className="text-primary-foreground font-bold"
+                    delay={0.5}
+                  />
+                  <span className="text-primary-foreground font-bold">%</span>
+                  <span className="text-primary-foreground font-bold">)</span>
+                </div>
+              </div>
+            </Progress>
+            <div className="flex w-full items-center justify-between px-1 font-medium">
+              <span>0명</span>
+              <span>목표 1,000명</span>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>
-        <Button>시작하기</Button>
+      <div className="mt-20 flex flex-wrap items-center justify-center gap-x-16 gap-y-8">
+        {[
+          "/naver-logo.svg",
+          "/Yahoo!_Finance_logo.svg",
+          "/naver-logo.svg",
+          "/Yahoo!_Finance_logo.svg",
+          "/naver-logo.svg",
+        ].map((logo, idx) => (
+          <img
+            key={idx}
+            src={logo}
+            alt="brand-logo"
+            className="h-8 w-auto opacity-40 grayscale transition-all duration-300 hover:opacity-100 hover:grayscale-0 dark:opacity-30 dark:invert dark:hover:opacity-100 dark:hover:brightness-0 dark:hover:invert"
+          />
+        ))}
+      </div>
+      {/* 기능 섹션 */}
+      <div className="mt-52 flex flex-col gap-6">
+        <div className="grid grid-cols-3">
+          <div className="col-span-1 flex flex-col justify-center">
+            <h2 className="text-[42px] font-semibold tracking-tight">제목</h2>
+            <TextAnimate
+              animation="slideLeft"
+              by="character"
+              className="text-muted-foreground"
+            >
+              내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+            </TextAnimate>
+          </div>
+          <div className="col-span-2 flex items-center justify-end pl-10">
+            <img
+              src="/routine-lifestyle.png"
+              alt="Routine Lifestyle"
+              className="border-border/40 aspect-[736/750] w-full max-w-[736px] rounded-[2rem] border object-cover"
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-3">
+          <div className="col-span-1 flex flex-col justify-center">
+            <h2 className="text-[42px] font-semibold tracking-tight">제목</h2>
+            <TextAnimate
+              animation="slideLeft"
+              by="character"
+              className="text-muted-foreground"
+            >
+              내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+            </TextAnimate>
+          </div>
+          <div className="col-span-2 flex items-center justify-end pl-10">
+            <img
+              src="/routine-lifestyle.png"
+              alt="Routine Lifestyle"
+              className="border-border/40 aspect-[736/750] w-full max-w-[736px] rounded-[2rem] border object-cover"
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-3">
+          <div className="col-span-1 flex flex-col justify-center">
+            <h2 className="text-[42px] font-semibold tracking-tight">제목</h2>
+            <TextAnimate
+              animation="slideLeft"
+              by="character"
+              className="text-muted-foreground"
+            >
+              내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
+            </TextAnimate>
+          </div>
+          <div className="col-span-2 flex items-center justify-end pl-10">
+            <img
+              src="/routine-lifestyle.png"
+              alt="Routine Lifestyle"
+              className="border-border/40 aspect-[736/750] w-full max-w-[736px] rounded-[2rem] border object-cover"
+            />
+          </div>
+        </div>
+        <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
+          <Marquee className="[--duration:20s]">
+            {firstRow.map((review) => (
+              <ReviewCard key={review.username} {...review} />
+            ))}
+          </Marquee>
+          <div className="from-background pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r"></div>
+          <div className="from-background pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l"></div>
+        </div>
       </div>
     </>
   );
