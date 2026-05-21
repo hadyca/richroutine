e2d838c11c6649459923c5c0065a384d85085b46
hadyca <hadycas@gmail.com>
2026-05-22 08:20:54 +0900

    네이버 소유 확인

--- app/root.tsx
+++ app/root.tsx
@@ -182,6 +182,7 @@ function InnerLayout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="naver-site-verification" content="ad880f796dc5c46de770ed86ecacb5a59ca94e63" />
         <Meta />
         <Links />
         {isPreRendered ? (
